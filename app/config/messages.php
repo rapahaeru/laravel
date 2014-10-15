ed6a0879bbffdd7816f3c6def295e6592f558e96
cael <rapahaeru@gmail.com>
2014-10-15 20:25:11 -0300

    [Config] criado arquivo para hospedar todas as arrays de mensagens

---
+++ app/config/messages.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+	"user" => array(
+		"error" 	=> array( 
+						"cadastro" => "Houve um problema no cadastro do usuário.",
+						"remove" => "Houve um erro na exclusão do usuário" ),
+		
+		"success"	=> array( 
+						"cadastro" => "Usuário inserido com sucesso!",
+						"remove" => "Usuário removido com sucesso!" )
+		
+		)
+
+
+];
