fdcf732ddcd53da62fcfef475658b475d3bde55a
cael <rapahaeru@gmail.com>
2014-10-17 20:27:46 -0300

    Adicionadas novas mensagens padrão

--- app/config/messages.php
+++ app/config/messages.php
@@ -3,14 +3,24 @@
 return [
 	"user" => array(
 		"error" 	=> array( 
-						"cadastro" => "Houve um problema no cadastro do usuário.",
-						"remove" => "Houve um erro na exclusão do usuário" ),
+						"cadastro" 				=> "Houve um problema no cadastro do usuário.",
+						"form-update" 			=> "Houve um problema na atualização do usuário. Por favor tente novamente mais tarde.",
+						"not-identified" 		=> "Usuário não identificado.",
+						"notfound" 				=> "Usuário não encontrado.",
+						"remove" 				=> "Houve um erro na exclusão do usuário" ),
 		
 		"success"	=> array( 
-						"cadastro" => "Usuário inserido com sucesso!",
-						"remove" => "Usuário removido com sucesso!" )
+						"cadastro" 				=> "Usuário inserido com sucesso!",
+						"form-update"			=> "Usuário atualizado com sucesso!",
+						"remove" 				=> "Usuário removido com sucesso!" )
 		
-		)
+		),
+
+	"login" => array(
+		"error" 	=> array(
+						"mail" 					=> "E-mail não cadastrado",
+						"pass" 					=> "Senha ou usuário inválido")
+				)
 
 
 ];
