cff9fd0c8cf8e824ce6431069bb36ba8162076e8
cael <rapahaeru@gmail.com>
2014-11-19 18:53:40 -0200

    Helper : Adicionada função para formatar tamanho dos bytes

--- app/helpers/admin_helper.php
+++ app/helpers/admin_helper.php
@@ -23,3 +23,20 @@ function showAlerts($type,$message){
 }
 
 
+function display_filesize($filesize){
+    
+    if(is_numeric($filesize)){
+    $decr = 1024; $step = 0;
+    $prefix = array('Byte','KB','MB','GB','TB','PB');
+        
+    while(($filesize / $decr) > 0.9){
+        $filesize = $filesize / $decr;
+        $step++;
+    } 
+    return round($filesize,2).' '.$prefix[$step];
+    } else {
+
+    return 'NaN';
+    }
+    
+}
