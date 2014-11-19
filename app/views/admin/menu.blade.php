3315e786de22773ea01f216e1ebc17320ffc6ee1
cael <rapahaeru@gmail.com>
2014-11-19 18:54:37 -0200

    Admin : menu - Verificação de página atual

--- app/views/admin/menu.blade.php
+++ app/views/admin/menu.blade.php
@@ -2,17 +2,18 @@
     <!-- @endif -->
 
 <? if (Session::has('user')) : ?>
-  <ul class="nav nav-tabs left" role="tablist">
+  <ul class="nav nav-pills left" role="tablist">
     <li>
       <ul class="nav nav-pills">
-        <li class="active"><a href="#">Dash</a></li>
+        <li class="{{{ isset($url_current) && $url_current == 'admin' ? 'active' :'' }}}"><a href="{{ url('/admin') }}">Dash</a></li>
+        <li class="{{{ isset($url_current) && $url_current == 'admin/midias' ? 'active' :'' }}}"><a href="{{ url('/admin/midias') }}">Mídias</a></li>
         <!-- <li><a href="#">Profile</a></li>
         <li><a href="#">Messages</a></li> -->
       </ul>      
     </li>
   </ul>
 
-  <ul class="nav nav-tabs right" role="tablist">
+  <ul class="nav nav-pills right" role="tablist">
     <li class="dropdown">
       <a class="dropdown-toggle" data-toggle="dropdown" href="#">
         Configurações <span class="caret"></span>
@@ -20,7 +21,7 @@
       <ul class="dropdown-menu" role="menu">
           <li><a href="{{ url('/admin/usuarios') }}">Usuários</a></li>
           <li class="divider"></li>
-          <li><a href="/admin/logout">Logout</a></li>
+          <li><a href="{{ url('/admin/logout') }}">Logout</a></li>
       </ul>
     </li>
   </ul>
